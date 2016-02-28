d804c395829790e0b984d96da6745b823c735558
Saulius Grigaitis <saulius@necolt.com>
2016-02-28 22:17:04 +0200

    Updated Stencil to 0.5.3

--- Package.swift
+++ Package.swift
@@ -6,7 +6,7 @@ let package = Package(
         Target(name: "Spec", dependencies: [.Target(name: "Swifton")]),
     ],
     dependencies: [
-        .Package(url: "https://github.com/necolt/Stencil.git", versions: Version(0,5,2)..<Version(1,0,0)),
+        .Package(url: "https://github.com/kylef/Stencil.git", versions: Version(0,5,3)..<Version(1,0,0)),
         .Package(url: "https://github.com/necolt/Inquiline.git", versions: Version(0,2,2)..<Version(1,0,0)),
         .Package(url: "https://github.com/necolt/URITemplate.swift.git", versions: Version(1,3,1)..<Version(2,0,0)),
         .Package(url: "https://github.com/briancroom/Quick.git", majorVersion: 0, minor: 9),
