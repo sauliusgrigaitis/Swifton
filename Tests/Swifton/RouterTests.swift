e61b8fe8176b63989a9db3ebc4864307b63820b9
Scott Gauthreaux <me@scottgauthreaux.com>
2016-03-27 11:23:32 -0700

    Fixed XCTest

--- Tests/Swifton/RouterTests.swift
+++ Tests/Swifton/RouterTests.swift
@@ -96,7 +96,7 @@ class RouterTests: SwiftonTest {
     func testMimeType() {
         request.path = "/static.js"
         let staticFile = router.respond(request)
-        XTAssetrEqual(staticFile.contentType, "application/javascript; charset=utf8")
+        XTAssertEqual(staticFile.contentType, "application/javascript; charset=utf8")
     }
 
 //    TODO: Fix
