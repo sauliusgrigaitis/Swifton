81c60f4de2ae1159b2f1935de1ef3232af53d266
Saulius Grigaitis <saulius@necolt.com>
2016-03-31 00:12:03 +0300

    Preparing for last build supporting Swift 2.2. Swift 3 is the future!

--- Package.swift
+++ Package.swift
@@ -3,8 +3,8 @@ import PackageDescription
 let package = Package(
     name: "Swifton",
     dependencies: [
-        .Package(url: "https://github.com/necolt/Stencil.git", versions: Version(0,5,3)..<Version(1,0,0)),
-        .Package(url: "https://github.com/necolt/Inquiline.git", versions: Version(0,2,2)..<Version(1,0,0)),
-        .Package(url: "https://github.com/necolt/URITemplate.swift.git", versions: Version(1,3,1)..<Version(2,0,0))
+        .Package(url: "https://github.com/necolt/Stencil.git", versions: Version(0,5,3)..<Version(0,5,4)),
+        .Package(url: "https://github.com/necolt/Inquiline.git", versions: Version(0,2,2)..<Version(0,2,3)),
+        .Package(url: "https://github.com/necolt/URITemplate.swift.git", versions: Version(1,3,1)..<Version(1,3,2))
     ]
 )
