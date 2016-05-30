7f0b9392a1ba0c2e26792d51a36cbd3d33ec5d84
Scott Gauthreaux <me@scottgauthreaux.com>
2016-05-29 19:19:33 -0700

    Added mime-type appropriate response for text files
    The `ContentType` enum has been updated to support all the mime-types
    in `MimeType.swift` in `Response.swift`
    
    The router has also bee updated to automatically return the correct
    content-type based on the file extension.

--- Sources/Swifton/Router.swift
+++ Sources/Swifton/Router.swift
@@ -34,12 +34,15 @@ extension Router {
 
         do {
             let contents = try filePath.read()
+            var contentType = ContentType.Plain
+            if let fileExtension = filePath.`extension`, let contentTypeRaw = Swifton.mimeTypes[fileExtension], let resolvedContentType = ContentType(rawValue: contentTypeRaw) {
+                contentType = resolvedContentType
+            }
+            print(contentType)
             if let body = String(data: contents, encoding: NSUTF8StringEncoding) {
-                return Response(status: .ok, body: body)
+                return Response(status: .ok, contentType: contentType, body: body)
             }
         } catch {
-            return Response(status: .notFound, body: "Error Reading From File")
-        }
 
         return nil
     }
