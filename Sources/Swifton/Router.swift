81caf9f22ff66f8ec9888cbb2bd9d8cae926f699
Scott Gauthreaux <me@scottgauthreaux.com>
2016-06-05 22:21:42 -0700

    Fixed router and deleted debug code.

--- Sources/Swifton/Router.swift
+++ Sources/Swifton/Router.swift
@@ -38,11 +38,12 @@ extension Router {
             if let fileExtension = filePath.`extension`, let contentTypeRaw = Swifton.mimeTypes[fileExtension], let resolvedContentType = ContentType(rawValue: contentTypeRaw) {
                 contentType = resolvedContentType
             }
-            print(contentType)
             if let body = String(data: contents, encoding: NSUTF8StringEncoding) {
                 return Response(status: .ok, contentType: contentType, body: body)
             }
         } catch {
+            return Response(status: .notFound, body: "Error Reading From File")
+        }
 
         return nil
     }
